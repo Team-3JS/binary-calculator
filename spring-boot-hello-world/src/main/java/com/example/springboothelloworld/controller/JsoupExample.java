80ab0b0a31c824b4fc40a7e77cd24b5df894f8b0
josue0128 <josuec@cpp.edu>
2022-10-06 20:42:31 -0800

    Added a new java project using Jsoup library

---
+++ spring-boot-hello-world/src/main/java/com/example/springboothelloworld/controller/JsoupExample.java
@@ -0,0 +1,37 @@
+package com.codacum.springboothelloworld;
+
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class JsoupExample {
+   public static void main(String[] args) {
+	   String html = "<html><head><title>First parse</title></head>"
+			   + 		"<body>"
+			   + 			"<h1>Getting Inputs from HTML using Jsoup</h1>"
+			   +          	"<div>"
+			   + 				"<h2>contents are here</h2>"
+			   +				"<form id=myform >"
+			   +                    "<input type=number name=number1 id=number1 value=10><br>"
+			   + 					"<input type=number name=number2 id=number2 value=20>"
+			   + 			"</div>"
+			   +    	"</body>"
+			   +    "</html>";
+			 
+	   Document doc = Jsoup.parse(html);
+	   Element form = doc.getElementById("myform");
+	   Element input1 = form.getElementById("number1");
+	   Element input2 = form.getElementById("number2");
+	   
+	   int num1 = Integer.parseInt(input1.attr("value"));
+	   int num2 = Integer.parseInt(input2.attr("value"));
+	   
+	   System.out.println("input 1: "+num1);
+	   System.out.println("input 2: "+num2);
+	   System.out.println("");
+	   System.out.println("sum of numbers: " + (num1 + num2) );
+	   
+   }
+}
