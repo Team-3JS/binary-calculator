fa3a97113f97c53e1e8c5d6d3215655d002e7950
Jhelsing1 <frostproxiide@gmail.com>
2022-10-15 20:32:34 -0700

    Updated int to binary method and testing junit class

--- spring-boot-hello-world/src/main/java/com/example/springboothelloworld/BinaryCalculations.java
+++ spring-boot-hello-world/src/main/java/com/example/springboothelloworld/BinaryCalculations.java
@@ -4,33 +4,77 @@ public class BinaryCalculations
 {
     public String intToBinary(int a)
     {
-        int b = a;
-        String retval = "";
-        while(b!= 0)
+        if(a > 0)
         {
-            if(b %2 == 1)
+            int b = a;
+            String retval = "";
+            while(b!= 0)
             {
-                retval = "1" + retval;
-                b--;
-                b/=2;
+                if(b %2 == 1)
+                {
+                    retval = "1" + retval;
+                    b--;
+                    b/=2;
+                }
+                else
+                {
+                    retval = "0" + retval;
+                    b/=2;
+                }
             }
-            else
+            retval = "0" + retval;
+            return retval;
+        }
+        else if(a < 0)
+        {
+            int b = a * -1;
+            int firstval = 1;
+            int places = 1;
+            while(firstval< b)
+            {
+                places ++;
+                firstval*=2;
+            }
+
+            b = firstval - b;
+            String retval = "";
+            while(b!= 0)
+            {
+                if(b %2 == 1)
+                {
+                    retval = "1" + retval;
+                    b--;
+                    b/=2;
+                }
+                else
+                {
+                    retval = "0" + retval;
+                    b/=2;
+                }
+            }
+
+            while(retval.length() < places -1)
             {
                 retval = "0" + retval;
-                b/=2;
             }
+
+            retval = "1" + retval;
+            return retval;
+
         }
-        retval = "0" + retval;
-        return retval;
+        else
+        {
+            return "00";
+        }
     }
 
-    /* 
+     /*
     public static void main(String[] args)
     {
         BinaryCalculations a = new BinaryCalculations();
         String h = a.intToBinary(14);
 
-        if(h == "01110")
+        if(h.equals("01110"))
         {
             System.out.println("correct test");
         }
@@ -41,7 +85,18 @@ public class BinaryCalculations
 
         String h2 = a.intToBinary(16);
 
-        if(h == "010000")
+        if(h2.equals("010000"))
+        {
+            System.out.println("correct test");
+        }
+        else
+        {
+            throw new RuntimeException("error in test");
+        }
+
+        String h3 = a.intToBinary(-10);
+
+        if(h3.equals("10110"))
         {
             System.out.println("correct test");
         }
--- spring-boot-hello-world/src/test/java/com/example/springboothelloworld/BinaryCalculationsTest.java
+++ spring-boot-hello-world/src/test/java/com/example/springboothelloworld/BinaryCalculationsTest.java
@@ -12,4 +12,20 @@ public class BinaryCalculationsTest
         String h = a.intToBinary(14);
         Assert.assertEquals("01110", h);
     }
+
+    @Test
+    public void testVal2()
+    {
+        BinaryCalculations a = new BinaryCalculations();
+        String h = a.intToBinary(16);
+        Assert.assertEquals("010000", h);
+    }
+
+    @Test
+    public void testVal3()
+    {
+        BinaryCalculations a = new BinaryCalculations();
+        String h = a.intToBinary(-10);
+        Assert.assertEquals("10110", h);
+    }
 }
