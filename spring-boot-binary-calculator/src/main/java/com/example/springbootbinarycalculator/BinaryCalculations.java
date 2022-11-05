418b9b8460782e6d3a184d454743a77f84bd2c31
Jhelsing1 <frostproxiide@gmail.com>
2022-11-05 13:36:05 -0700

    Update BinaryCalculations.java

--- spring-boot-binary-calculator/src/main/java/com/example/springbootbinarycalculator/BinaryCalculations.java
+++ spring-boot-binary-calculator/src/main/java/com/example/springbootbinarycalculator/BinaryCalculations.java
@@ -68,37 +68,40 @@ public class BinaryCalculations
         }
     }
 
-    public static String binaryAdd(String binaryNum1, String binaryNum2){
+    public static String Add(String Num1, String Num2, int base){
         String result;
-        int integerResult = Integer.parseInt(binaryNum1, 2) + Integer.parseInt(binaryNum2, 2);
-        result = Integer.toBinaryString(integerResult);
+        int integerResult = Integer.parseInt(Num1, base) + Integer.parseInt(Num2, base);
+        result = Integer.toString(integerResult ,base);
         return result;
     }
 
-    public static String binarySubtract(String binaryNum1, String binaryNum2){
+    public static String Subtract(String Num1, String Num2, int base){
         String result;
-        int integerResult = Integer.parseInt(binaryNum1, 2) - Integer.parseInt(binaryNum2, 2);
-        result = Integer.toBinaryString(integerResult);
+        int integerResult = Integer.parseInt(Num1, base) - Integer.parseInt(Num2, base);
+        result = Integer.toString(integerResult ,base);
         return result;
     }
 
-    public static String binaryMultiply(String binaryNum1, String binaryNum2){
+    public static String Multiply(String Num1, String Num2, int base){
         String result;
-        int integerResult = Integer.parseInt(binaryNum1, 2) * Integer.parseInt(binaryNum2, 2);
-        result = Integer.toBinaryString(integerResult);
+        int integerResult = Integer.parseInt(Num1, base) * Integer.parseInt(Num2, base);
+        result = Integer.toString(integerResult ,base);
         return result;
     }
 
-    public static String binaryDivide(String binaryNum1, String binaryNum2){
+    public static String Divide(String Num1, String Num2, int base){
         String result;
-        if(Integer.parseInt(binaryNum2) == 0)
+        if(Integer.parseInt(Num2) == 0)
             return "Cannot divide by zero";
 
-        int integerResult = Integer.parseInt(binaryNum1, 2) / Integer.parseInt(binaryNum2, 2);
-        result = Integer.toBinaryString(integerResult);
+        int integerResult = Integer.parseInt(Num1, base) / Integer.parseInt(Num2, base);
+        result = Integer.toString(integerResult ,base);
         return result;
     }
 
+    public static String toString(String Num, int origBase, int newBase){
+        return Integer.toString(Integer.parseInt(Num, origBase), newBase);
+    }
      /*
     public static void main(String[] args)
     {
